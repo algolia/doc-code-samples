c6a63c9ab3bb0030f70896029d6a784a35e360f5
vvo <vincent.voyer@algolia.com>
2018-06-15 12:27:15 +0200

    feat(Angular): add extending widgets example

---
+++ Angular InstantSearch/extending-widgets/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgAisModule } from 'angular-instantsearch';
+
+import { AppComponent } from './app.component';
+import { MenuSelect } from './menu-select.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuSelect
+  ],
+  imports: [
+    NgAisModule.forRoot(),
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular InstantSearch/extending-widgets/src/app/menu-select.component.ts
@@ -0,0 +1,44 @@
+import { Component, Inject, forwardRef } from '@angular/core';
+import { BaseWidget, NgAisInstantSearch } from 'angular-instantsearch';
+import { connectMenu } from 'instantsearch.js/es/connectors';
+
+@Component({
+  selector: 'ais-menu-select',
+  template: `
+    <select
+      class="menu-select"
+      (change)="state.refine($event.target.value)"
+    >
+      <option
+        *ngFor="let item of state.items"
+        [value]="item.value"
+        [selected]="item.isRefined"
+      >
+        {{item.label}}
+      </option>
+    </select>
+  `
+})
+export class MenuSelect extends BaseWidget {
+  state: {
+    items: { label: string; value: string }[];
+    createURL: () => string;
+    refine: (value: string) => void;
+    canRefine: boolean;
+    isShowingMore: boolean;
+    toggleShowMore: () => void;
+    canToggleShowMore: boolean;
+  }
+
+  constructor(
+    @Inject(forwardRef(() => NgAisInstantSearch))
+    public instantSearchParent
+  ) {
+    super('MenuSelect');
+  }
+
+  public ngOnInit() {
+    this.createWidget(connectMenu, { attributeName: 'categories' });
+    super.ngOnInit();
+  }
+}
