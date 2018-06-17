e062dc25270106189522004e558cda1fdcde661b
vvo <vincent.voyer@algolia.com>
2018-06-17 22:21:56 +0200

    feat(Angular): add lists manipulations samples

---
+++ Angular InstantSearch/manipulating-lists/filtering/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { sortBy } from 'lodash';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  config = {
+    appId: 'B1G2GM9NG0',
+    apiKey: 'aadef574be1f9252bb48d4ea09b5cfe5',
+    indexName: 'demo_ecommerce'
+  };
+  removeSamsung(items) {
+    return items.filter(item => item.label === 'Samsung')
+  }
+}
