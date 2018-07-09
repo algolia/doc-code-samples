52d383404268d227cc48ae0d6f75ecc055f6367a
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-07-09 11:38:07 +0200

    feat(react): add extending widgets example

---
+++ React InstantSearch/extending-widgets/src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Highlight,
+  ClearRefinements,
+  Configure,
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
+import MenuSelect from './MenuSelect';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ais-InstantSearch">
+        <h1>React InstantSearch e-commerce demo</h1>
+        <InstantSearch
+          appId="B1G2GM9NG0"
+          apiKey="aadef574be1f9252bb48d4ea09b5cfe5"
+          indexName="demo_ecommerce"
+        >
+          <div className="left-panel">
+            <ClearRefinements />
+            <h2>Brands</h2>
+            <MenuSelect attribute="categories" />
+            <Configure hitsPerPage={4} />
+          </div>
+          <div className="right-panel">
+            <SearchBox />
+            <Hits hitComponent={Hit} />
+          </div>
+        </InstantSearch>
+      </div>
+    );
+  }
+}
+
+function Hit(props) {
+  return (
+    <div>
+      <img src={props.hit.image} align="left" alt={props.hit.name} />
+      <div className="hit-name">
+        <Highlight attribute="name" hit={props.hit} />
+      </div>
+      <div className="hit-description">
+        <Highlight attribute="description" hit={props.hit} />
+      </div>
+      <div className="hit-price">${props.hit.price}</div>
+    </div>
+  );
+}
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
+
+export default App;
