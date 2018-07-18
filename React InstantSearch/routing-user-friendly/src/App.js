bcfa217ac6ebe91d27cd75061482d6e02fb9715d
François Chalifour <francois.chalifour@gmail.com>
2018-07-18 14:15:55 +0200

    refactor(react): Use `searchClient` in routing examples

--- React InstantSearch/routing-user-friendly/src/App.js
+++ React InstantSearch/routing-user-friendly/src/App.js
@@ -7,6 +7,7 @@ import {
   Pagination,
   Highlight,
 } from 'react-instantsearch-dom';
+import algoliasearch from 'algoliasearch';
 import qs from 'qs';
 import PropTypes from 'prop-types';
 import './App.css';
@@ -76,8 +77,10 @@ class App extends Component {
     return (
       <div className="container">
         <InstantSearch
-          appId="latency"
-          apiKey="6be0576ff61c053d5f9a3225e2a90f76"
+          searchClient={algoliasearch(
+            'latency',
+            '6be0576ff61c053d5f9a3225e2a90f76'
+          )}
           indexName="instant_search"
           searchState={this.state.searchState}
           onSearchStateChange={this.onSearchStateChange}
