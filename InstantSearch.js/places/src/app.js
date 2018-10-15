b8b3a05ef4974766f3e303da29b0263e8a543456
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-10-15 18:01:33 +0200

    feat(js): places

---
+++ InstantSearch.js/places/src/app.js
@@ -0,0 +1,31 @@
+/* global instantsearch algoliasearch $script placesInstantsearchWidget */
+
+$script(
+  'https://maps.googleapis.com/maps/api/js?v=weekly&key=AIzaSyBawL8VbstJDdU5397SUX7pEt9DslAwWgQ',
+  () => {
+    const searchClient = algoliasearch(
+      'latency',
+      '6be0576ff61c053d5f9a3225e2a90f76'
+    );
+
+    const search = instantsearch({
+      indexName: 'airbnb',
+      searchClient,
+    });
+
+    search.addWidget(
+      placesInstantsearchWidget({
+        container: '#searchbox',
+      })
+    );
+
+    search.addWidget(
+      instantsearch.widgets.geoSearch({
+        container: '#maps',
+        googleReference: window.google,
+      })
+    );
+
+    search.start();
+  }
+);
