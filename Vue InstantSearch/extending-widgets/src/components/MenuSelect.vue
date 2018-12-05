898920b8cbdd734d7f8558c8b2c954142a6d91c5
Haroen Viaene <hello@haroen.me>
2018-12-05 11:38:27 +0100

    feat(vue): add example of custom widget

---
+++ Vue InstantSearch/extending-widgets/src/components/MenuSelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <ais-menu :attribute="attribute">
+    <select
+      slot-scope="{ items, canRefine, refine }"
+      @change="refine($event.currentTarget.value)"
+      :disabled="!canRefine"
+    >
+      <option value=""> All </option>
+      <option
+        v-for="item in items"
+        :key="item.value"
+        :value="item.value"
+        :selected="item.isRefined"
+      >
+        {{ item.label }}
+      </option>
+    </select>
+  </ais-menu>
+</template>
+
+<script>
+export default {
+  props: {
+    attribute: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
