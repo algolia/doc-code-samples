d5467e5f38795e3b0ad48c37bc0c04972d6a9e51
Haroen Viaene <hello@haroen.me>
2018-12-10 18:11:49 +0100

    chore: inline component

--- Vue InstantSearch/autocomplete-results-page/src/App.vue
+++ Vue InstantSearch/autocomplete-results-page/src/App.vue
@@ -19,7 +19,42 @@
           :hitsPerPage="5"
           :restrictSearchableAttributes="['name']"
         />
-        <app-autocomplete />
+        <ais-autocomplete>
+          <template slot-scope="{ currentRefinement, indices, refine }">
+            <vue-autosuggest
+              :suggestions="indicesToSuggestions(indices)"
+              @selected="onSelect"
+              :input-props="{
+                style: 'width: 100%',
+                onInputChange: refine,
+                placeholder: 'Search here…',
+              }"
+            >
+              <template slot-scope="{ suggestion }">
+                <ais-highlight
+                  :hit="suggestion.item"
+                  attribute="name"
+                  v-if="suggestion.item.name"
+                />
+                <strong>$ {{ suggestion.item.price }}</strong>
+                <img :src="suggestion.item.image" />
+              </template>
+            </vue-autosuggest>
+          </template>
+        </ais-autocomplete>
+      </ais-instant-search>
+      <ais-instant-search
+        :search-client="searchClient"
+        index-name="demo_ecommerce"
+      >
+        <ais-configure :query="query" />
+        <ais-hits>
+          <div slot="item" slot-scope="{ item }">
+            <ais-highlight :hit="item" attribute="name" v-if="item.name" />
+            <strong>$ {{ item.price }}</strong> <img :src="item.image" />
+          </div>
+        </ais-hits>
+        <ais-pagination />
       </ais-instant-search>
     </div>
   </div>
@@ -28,17 +63,31 @@
 <script>
 import algoliasearch from 'algoliasearch/lite';
 import 'instantsearch.css/themes/algolia-min.css';
-import AppAutocomplete from './components/Autocomplete';
+import { VueAutosuggest } from 'vue-autosuggest';
 
 export default {
-  components: { AppAutocomplete },
+  components: { VueAutosuggest },
   data() {
     return {
       searchClient: algoliasearch(
         'B1G2GM9NG0',
         'aadef574be1f9252bb48d4ea09b5cfe5'
       ),
+      query: '',
     };
+  },
+  methods: {
+    onSelect(selected) {
+      if (selected) {
+        this.query = selected.item.name;
+      }
+    },
+    indicesToSuggestions(indices) {
+      return indices.map(({ hits }) => ({ data: hits }));
+    },
+    test(event) {
+      console.log(event)
+    }
   },
 };
 </script>
@@ -93,5 +142,36 @@ body {
   max-width: 1200px;
   margin: 0 auto;
   padding: 1rem;
+}
+
+#autosuggest input {
+  font: inherit;
+}
+
+.autosuggest__results-container {
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+
+.autosuggest__results-container ul {
+  margin: 0;
+  padding: 0;
+}
+
+.autosuggest__results_item {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+  list-style-type: none;
+  padding: 0.5em;
+  display: grid;
+  grid-template-columns: 5fr 1fr 1fr;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.autosuggest__results_item img {
+  height: 3em;
+}
+
+.autosuggest__results_item-highlighted {
+  background-color: rgba(0, 0, 0, 0.24);
 }
 </style>
