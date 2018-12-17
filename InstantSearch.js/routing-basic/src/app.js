f3ebb5bc0cf9ae793fdcd4a80117bf62c817bce5
François Chalifour <francois.chalifour@gmail.com>
2018-12-17 15:22:42 +0100

    feat(js): update example

--- InstantSearch.js/routing-basic/src/app.js
+++ InstantSearch.js/routing-basic/src/app.js
@@ -3,27 +3,7 @@
 const search = instantsearch({
   indexName: 'instant_search',
   searchClient: algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76'),
-  routing: {
-    stateMapping: {
-      stateToRoute(uiState) {
-        return {
-          query: uiState.query,
-          brands:
-            uiState.refinementList && uiState.refinementList.brand.join('~'),
-          page: uiState.page,
-        };
-      },
-      routeToState(routeState) {
-        return {
-          query: routeState.query,
-          refinementList: {
-            brand: routeState.brands && routeState.brands.split('~'),
-          },
-          page: routeState.page,
-        };
-      },
-    },
-  },
+  routing: true,
 });
 
 search.addWidget(
