81a9242a96b88e3c96d74e689c28870372d41f27
Haroen Viaene <hello@haroen.me>
2018-12-18 09:23:47 +0100

    fix(vue): correct sample in routing

--- Vue InstantSearch/routing-vue-router/src/views/Home.vue
+++ Vue InstantSearch/routing-vue-router/src/views/Home.vue
@@ -51,39 +51,39 @@ export default {
         'aadef574be1f9252bb48d4ea09b5cfe5'
       ),
       routing: {
-  router: {
-    read() {
-      return vueRouter.currentRoute.query;
-    },
-    write(routeState) {
-      vueRouter.push({
-        query: routeState,
-      });
-    },
-    createURL(routeState) {
-      return vueRouter.resolve({
-        query: routeState,
-      }).href;
-    },
-    onUpdate() {
-      this._onPopState = event => {
-        const routeState = event.state;
-        // at initial load, the state is read from the URL without
-        // update. Therefore the state object is not there. In this
-        // case we fallback and read the URL.
-        if (!routeState) {
-          cb(this.read());
-        } else {
-          cb(routeState);
-        }
-      };
-      window.addEventListener('popstate', this._onPopState);
-    },
-    dispose() {
-      window.removeEventListener('popstate', this._onPopState);
-      this.write();
-    },
-  },
+        router: {
+          read() {
+            return vueRouter.currentRoute.query;
+          },
+          write(routeState) {
+            vueRouter.push({
+              query: routeState,
+            });
+          },
+          createURL(routeState) {
+            return vueRouter.resolve({
+              query: routeState,
+            }).href;
+          },
+          onUpdate(cb) {
+            this._onPopState = event => {
+              const routeState = event.state;
+              // at initial load, the state is read from the URL without
+              // update. Therefore the state object is not there. In this
+              // case we fallback and read the URL.
+              if (!routeState) {
+                cb(this.read());
+              } else {
+                cb(routeState);
+              }
+            };
+            window.addEventListener('popstate', this._onPopState);
+          },
+          dispose() {
+            window.removeEventListener('popstate', this._onPopState);
+            this.write();
+          },
+        },
         stateMapping: {
           stateToRoute(uiState) {
             return {
