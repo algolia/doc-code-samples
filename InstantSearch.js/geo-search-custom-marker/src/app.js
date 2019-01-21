4a072a3adc4e3b42ba3d1a7ad600a4663238baec
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-01-21 10:54:43 +0100

    feat(js): add geo search with custom marker

---
+++ InstantSearch.js/geo-search-custom-marker/src/app.js
@@ -0,0 +1,40 @@
+/* global algoliasearch instantsearch */
+
+import injectScript from 'scriptjs';
+
+injectScript(
+  'https://maps.googleapis.com/maps/api/js?v=quarterly&key=AIzaSyBawL8VbstJDdU5397SUX7pEt9DslAwWgQ',
+  () => {
+    const searchClient = algoliasearch(
+      'latency',
+      '6be0576ff61c053d5f9a3225e2a90f76'
+    );
+
+    const search = instantsearch({
+      indexName: 'airports',
+      searchClient,
+    });
+
+    search.addWidget(
+      instantsearch.widgets.searchBox({
+        container: '#searchbox',
+      })
+    );
+
+    search.addWidget(
+      instantsearch.widgets.geoSearch({
+        container: '#maps',
+        googleReference: window.google,
+        templates: {
+          HTMLMarker: `
+            <span class="marker">
+              {{ city }} - {{ airport_id }}
+            </span>
+          `,
+        },
+      })
+    );
+
+    search.start();
+  }
+);
