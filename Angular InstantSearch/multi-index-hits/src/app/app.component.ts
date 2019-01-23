2d7167a4e8022f492bfd8aea00425556ea189a92
Youcef Mammar <youcef.mammar@algolia.com>
2019-01-23 11:08:38 +0100

    add angular/multi-index-hits

---
+++ Angular InstantSearch/multi-index-hits/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import * as algoliasearch from 'algoliasearch';
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private query : string = "";
+
+  public configProducts = {
+    indexName: 'players',
+    searchClient,
+  };
+
+  public configActors = {
+    indexName: 'actors',
+    searchClient,
+  };
+
+  onQuery($event) {
+    this.query = $event.target.value
+  }
+
+  get searchParameters() {
+    return {
+      query: this.query
+    }
+  }
+}
