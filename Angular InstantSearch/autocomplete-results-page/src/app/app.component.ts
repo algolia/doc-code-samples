ba697a625239b1c9588c9986ccd0945b5d28e6de
Youcef Mammar <youcef.mammar@algolia.com>
2019-01-29 19:51:09 +0100

    add angular/autocomplete-results-page

---
+++ Angular InstantSearch/autocomplete-results-page/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+import * as algoliasearch from "algoliasearch";
+
+const searchClient = algoliasearch(
+  "B1G2GM9NG0",
+  "aadef574be1f9252bb48d4ea09b5cfe5"
+);
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  config = {
+    indexName: "demo_ecommerce",
+    searchClient
+  };
+
+  public searchParameters = {
+    query: ""
+  };
+
+  public setQuery({ query } : {query: string}) {
+    this.searchParameters.query = query;
+  }
+}
