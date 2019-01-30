56bce75659f9306f94726b25d4fde1971cab13fe
Youcef Mammar <youcef.mammar@algolia.com>
2019-01-30 01:31:18 +0100

    add angular/routing-angular-router

---
+++ Angular InstantSearch/routing-angular-router/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgAisModule } from "angular-instantsearch";
+
+import { RouterModule, Routes } from "@angular/router";
+
+import { SearchComponent } from "./search/search.component";
+import { IndexComponent } from "./index/index.component";
+
+const appRoutes: Routes = [
+  { path: "search", component: SearchComponent },
+  { path: "", component: IndexComponent }
+];
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent, SearchComponent, IndexComponent],
+  imports: [NgAisModule.forRoot(), BrowserModule, RouterModule.forRoot(appRoutes, { enableTracing: true })],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
