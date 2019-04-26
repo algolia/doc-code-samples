ae1fdfcd674384416106dfbf4a7548c500db5c66
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-04-26 12:39:29 +0200

    feat(angular): add conditional request

---
+++ Angular InstantSearch/conditional-request/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import * as algoliasearch from 'algoliasearch/lite';
+
+const algoliaClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+const searchClient = {
+  search(requests) {
+    if (requests.every(({ params }) => Boolean(params.query) === false)) {
+      return Promise.resolve({
+        results: requests.map(() => ({
+          hits: [],
+          nbHits: 0,
+          processingTimeMS: 0,
+        })),
+      });
+    }
+
+    return algoliaClient.search(requests);
+  },
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public config = {
+    indexName: 'instant_search',
+    searchClient,
+  };
+}
---
+++ Angular InstantSearch/conditional-request/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgAisModule } from 'angular-instantsearch';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [NgAisModule.forRoot(), BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
