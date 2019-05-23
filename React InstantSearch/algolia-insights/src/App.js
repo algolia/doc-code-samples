99e2586da753764e0e5ec3d18300912dfd2ea717
Youcef Mammar <tkrugg@users.noreply.github.com>
2019-05-23 14:53:36 -0700

    chore(React InstantSearch): use InstantSearch + Insights integration in algolia-insights sample (#105)
    * chore(React InstantSearch): use InstantSearch + Insights integration in algolia-insights sample
    
    Other changes are:
    - remove custom Hits component as no longer necessary
    - update search-insights to v1.2.0
    - update search-insights install snippets to extract source URL (as documented here: https://github.com/algolia/doc/pull/3063)
    
    * refactor: move Hit and HitWithInsights.js into their own file
    
    * fix: add insights func proptype on Hit

--- React InstantSearch/algolia-insights/src/App.js
+++ React InstantSearch/algolia-insights/src/App.js
@@ -1,19 +1,16 @@
 import algoliasearch from 'algoliasearch/lite';
 import React, { Component } from 'react';
 import {
-  InstantSearch,
-  SearchBox,
-  Pagination,
-  Highlight,
   ClearRefinements,
-  RefinementList,
   Configure,
-  connectStateResults,
-  connectHits,
+  Hits,
+  InstantSearch,
+  Pagination,
+  RefinementList,
+  SearchBox,
 } from 'react-instantsearch-dom';
-
-import PropTypes from 'prop-types';
 import './App.css';
+import { HitWithInsights } from './HitWithInsights';
 
 const searchClient = algoliasearch(
   'B1G2GM9NG0',
@@ -34,7 +31,7 @@ class App extends Component {
           </div>
           <div className="right-panel">
             <SearchBox />
-            <CustomHits />
+            <Hits hitComponent={HitWithInsights} />
             <Pagination />
           </div>
         </InstantSearch>
@@ -42,67 +39,5 @@ class App extends Component {
     );
   }
 }
-
-const CustomHits = connectHits(
-  connectStateResults(({ hits, searchResults }) => (
-    <div className="ais-Hits">
-      <ul className="ais-Hits-list">
-        {hits.map((hit, index) => (
-          <li className="ais-Hits-item" key={hit.objectID}>
-            <Hit hit={hit} searchResults={searchResults} index={index} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  ))
-);
-
-function Hit({ hit, index, searchResults }) {
-  return (
-    <div>
-      <img src={hit.image} align="left" alt={hit.name} />
-      <div className="hit-name">
-        <Highlight attribute="name" hit={hit} />
-      </div>
-      <div className="hit-description">
-        <Highlight attribute="description" hit={hit} />
-      </div>
-      <div className="hit-price">${hit.price}</div>
-      <button
-        className="hit-action"
-        onClick={() => {
-          window.aa('clickedObjectIDsAfterSearch', {
-            index: 'demo_ecommerce',
-            eventName: 'Add to favorite',
-            queryID: searchResults.queryID,
-            objectIDs: [hit.objectID],
-            positions: [
-              searchResults.hitsPerPage * searchResults.page + index + 1,
-            ],
-          });
-        }}
-      >
-        Send click
-      </button>{' '}
-      <button
-        className="hit-action"
-        onClick={() => {
-          window.aa('convertedObjectIDsAfterSearch', {
-            index: 'demo_ecommerce',
-            eventName: 'Add to basket',
-            queryID: searchResults.queryID,
-            objectIDs: [hit.objectID],
-          });
-        }}
-      >
-        Send conversion
-      </button>
-    </div>
-  );
-}
-
-Hit.propTypes = {
-  hit: PropTypes.object.isRequired,
-};
 
 export default App;
