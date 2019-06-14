9caa5bac32bd20ba4ac468f794e8be4731d4f61c
François Chalifour <francoischalifour@users.noreply.github.com>
2019-06-14 15:37:24 +0200

    feat(js): update SEO-friendly routing example (#102)

--- InstantSearch.js/routing-seo-friendly/src/app.js
+++ InstantSearch.js/routing-seo-friendly/src/app.js
@@ -1,90 +1,26 @@
 /* global instantsearch algoliasearch */
+import searchRouting from './search-routing';
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
 
 const search = instantsearch({
   indexName: 'instant_search',
-  searchClient: algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76'),
-  routing: {
-    router: instantsearch.routers.history({
-      windowTitle(routeState) {
-        return `Website / Find ${routeState.q} in ${routeState.brands} brands`;
-      },
-      createURL({ routeState, location }) {
-        let baseUrl = location.href.split('/search/')[0];
-
-        if (
-          !routeState.q &&
-          routeState.brands === 'all' &&
-          routeState.p === 1
-        ) {
-          return baseUrl;
-        }
-
-        if (baseUrl[baseUrl.length - 1] !== '/') {
-          baseUrl += '/';
-        }
-
-        const routeStateArray = [
-          'q',
-          encodeURIComponent(routeState.q),
-          'brands',
-          encodeURIComponent(routeState.brands),
-          'p',
-          routeState.p,
-        ];
-
-        return `${baseUrl}search/${routeStateArray.join('/')}`;
-      },
-      parseURL({ location }) {
-        const routeStateString = location.href.split('/search/')[1];
-
-        if (routeStateString === undefined) {
-          return {};
-        }
-
-        const routeStateValues = routeStateString.match(
-          /^q\/(.*?)\/brands\/(.*?)\/p\/(.*?)$/
-        );
-
-        return {
-          q: decodeURIComponent(routeStateValues[1]),
-          brands: decodeURIComponent(routeStateValues[2]),
-          p: routeStateValues[3],
-        };
-      },
-    }),
-    stateMapping: {
-      stateToRoute(uiState) {
-        return {
-          q: uiState.query || '',
-          brands:
-            (uiState.refinementList &&
-              uiState.refinementList.brand &&
-              uiState.refinementList.brand.join('~')) ||
-            'all',
-          p: uiState.page || 1,
-        };
-      },
-      routeToState(routeState) {
-        if (routeState.brands === 'all') {
-          // eslint-disable-next-line no-param-reassign
-          routeState.brands = undefined;
-        }
-
-        return {
-          query: routeState.q,
-          refinementList: {
-            brand: routeState.brands && routeState.brands.split('~'),
-          },
-          page: routeState.p,
-        };
-      },
-    },
-  },
+  searchClient,
+  routing: searchRouting,
 });
 
 search.addWidget(
   instantsearch.widgets.searchBox({
     container: '#searchbox',
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.clearRefinements({
+    container: '#clear-refinements',
   })
 );
 
@@ -101,8 +37,27 @@ search.addWidget(
   })
 );
 
+const categoryMenu = instantsearch.widgets.panel({
+  templates: {
+    header: 'Category',
+  },
+})(instantsearch.widgets.menu);
+
 search.addWidget(
-  instantsearch.widgets.refinementList({
+  categoryMenu({
+    container: '#menu',
+    attribute: 'categories',
+  })
+);
+
+const brandList = instantsearch.widgets.panel({
+  templates: {
+    header: 'Brands',
+  },
+})(instantsearch.widgets.refinementList);
+
+search.addWidget(
+  brandList({
     container: '#refinement-list',
     attribute: 'brand',
   })
---
+++ InstantSearch.js/routing-seo-friendly/src/search-routing.js
@@ -0,0 +1,132 @@
+/* global instantsearch */
+
+const encodedCategories = {
+  Cameras: 'Cameras & Camcorders',
+  Cars: 'Car Electronics & GPS',
+  Phones: 'Cell Phones',
+  TV: 'TV & Home Theater',
+};
+
+const decodedCategories = Object.keys(encodedCategories).reduce((acc, key) => {
+  const newKey = encodedCategories[key];
+  const newValue = key;
+
+  return {
+    ...acc,
+    [newKey]: newValue,
+  };
+}, {});
+
+// Returns a slug from the category name.
+// Spaces are replaced by "+" to make
+// the URL easier to read and other
+// characters are encoded.
+function getCategorySlug(name) {
+  const encodedName = decodedCategories[name] || name;
+
+  return encodedName
+    .split(' ')
+    .map(encodeURIComponent)
+    .join('+');
+}
+
+// Returns a name from the category slug.
+// The "+" are replaced by spaces and other
+// characters are decoded.
+function getCategoryName(slug) {
+  const decodedSlug = encodedCategories[slug] || slug;
+
+  return decodedSlug
+    .split('+')
+    .map(decodeURIComponent)
+    .join(' ');
+}
+
+const router = instantsearch.routers.history({
+  windowTitle({ category, query }) {
+    const queryTitle = query ? `Results for "${query}"` : 'Search';
+
+    if (category) {
+      return `${category} – ${queryTitle}`;
+    }
+
+    return queryTitle;
+  },
+
+  createURL({ qsModule, routeState, location }) {
+    const urlParts = location.href.match(/^(.*?)\/search/);
+    const baseUrl = `${urlParts ? urlParts[1] : ''}/`;
+
+    const categoryPath = routeState.category
+      ? `${getCategorySlug(routeState.category)}/`
+      : '';
+    const queryParameters = {};
+
+    if (routeState.query) {
+      queryParameters.query = encodeURIComponent(routeState.query);
+    }
+    if (routeState.page !== 1) {
+      queryParameters.page = routeState.page;
+    }
+    if (routeState.brands) {
+      queryParameters.brands = routeState.brands.map(encodeURIComponent);
+    }
+
+    const queryString = qsModule.stringify(queryParameters, {
+      addQueryPrefix: true,
+      arrayFormat: 'repeat',
+    });
+
+    return `${baseUrl}search/${categoryPath}${queryString}`;
+  },
+
+  parseURL({ qsModule, location }) {
+    const pathnameMatches = location.pathname.match(/search\/(.*?)\/?$/);
+    const category = getCategoryName(
+      (pathnameMatches && pathnameMatches[1]) || ''
+    );
+    const { query = '', page, brands = [] } = qsModule.parse(
+      location.search.slice(1)
+    );
+    // `qs` does not return an array when there's a single value.
+    const allBrands = Array.isArray(brands) ? brands : [brands].filter(Boolean);
+
+    return {
+      query: decodeURIComponent(query),
+      page,
+      brands: allBrands.map(decodeURIComponent),
+      category,
+    };
+  },
+});
+
+const stateMapping = {
+  stateToRoute(uiState) {
+    return {
+      query: uiState.query,
+      page: uiState.page,
+      brands: uiState.refinementList && uiState.refinementList.brand,
+      category: uiState.menu && uiState.menu.categories,
+    };
+  },
+
+  routeToState(routeState) {
+    return {
+      query: routeState.query,
+      page: routeState.page,
+      menu: {
+        categories: routeState.category,
+      },
+      refinementList: {
+        brand: routeState.brands,
+      },
+    };
+  },
+};
+
+const searchRouting = {
+  router,
+  stateMapping,
+};
+
+export default searchRouting;
