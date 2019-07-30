ae2b34b9b9c91bf0477c9d5e9fd403808224675e
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-07-30 15:16:26 +0200

    fix: update React Native Getting Started

--- React InstantSearch Native/getting-started/src/InfiniteHits.js
+++ React InstantSearch Native/getting-started/src/InfiniteHits.js
@@ -7,17 +7,21 @@ import Highlight from './Highlight';
 const styles = StyleSheet.create({
   separator: {
     borderBottomWidth: 1,
+    borderColor: '#ddd',
   },
   item: {
     padding: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  titleText: {
+    fontWeight: 'bold',
   },
 });
 
 const InfiniteHits = ({ hits, hasMore, refine }) => (
   <FlatList
     data={hits}
+    style
     keyExtractor={item => item.objectID}
     ItemSeparatorComponent={() => <View style={styles.separator} />}
     onEndReached={() => hasMore && refine()}
