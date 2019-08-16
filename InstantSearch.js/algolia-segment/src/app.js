9739216d9b1be022decd54643f320e05721d4f94
Eunjae Lee <eunjae.lee@algolia.com>
2019-08-16 17:49:46 +0200

    feat(js): add segment integration sample (by create-instantsearch-app)

---
+++ InstantSearch.js/algolia-segment/src/app.js
@@ -0,0 +1,39 @@
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+const search = instantsearch({
+  indexName: 'instant_search',
+  searchClient,
+});
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+        <article>
+          <h1>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</h1>
+          <p>{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</p>
+        </article>
+      `,
+    },
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  })
+);
+
+search.start();
