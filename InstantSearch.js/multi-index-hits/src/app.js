f8f66b672f7070bd300ba1f2c9a047babefd2656
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-09-27 10:16:35 +0200

    feat(js): update multi-index hits to 4.x.x

--- InstantSearch.js/multi-index-hits/src/app.js
+++ InstantSearch.js/multi-index-hits/src/app.js
@@ -5,57 +5,43 @@ const searchClient = algoliasearch(
   '6be0576ff61c053d5f9a3225e2a90f76'
 );
 
-const subIndex = instantsearch({
-  indexName: 'instant_search_price_desc',
-  searchClient,
-});
-
-subIndex.addWidget(
-  instantsearch.widgets.configure({
-    hitsPerPage: 16,
-  })
-);
-
-subIndex.addWidget(
-  instantsearch.widgets.hits({
-    container: '#hits-instant-search-price-desc',
-    templates: {
-      item:
-        '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
-    },
-  })
-);
-
-const mainIndex = instantsearch({
+const search = instantsearch({
   indexName: 'instant_search',
   searchClient,
-  searchFunction(helper) {
-    subIndex.helper.setQuery(helper.state.query).search();
-    helper.search();
-  },
 });
 
-mainIndex.addWidget(
+search.addWidgets([
   instantsearch.widgets.configure({
     hitsPerPage: 8,
-  })
-);
+  }),
 
-mainIndex.addWidget(
   instantsearch.widgets.searchBox({
     container: '#searchbox',
-  })
-);
+  }),
 
-mainIndex.addWidget(
   instantsearch.widgets.hits({
     container: '#hits-instant-search',
     templates: {
       item:
         '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
     },
-  })
-);
+  }),
 
-subIndex.start();
-mainIndex.start();
+  instantsearch.widgets
+    .index({ indexName: 'instant_search_price_desc' })
+    .addWidgets([
+      instantsearch.widgets.configure({
+        hitsPerPage: 16,
+      }),
+
+      instantsearch.widgets.hits({
+        container: '#hits-instant-search-price-desc',
+        templates: {
+          item:
+            '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
+        },
+      }),
+    ]),
+]);
+
+search.start();
