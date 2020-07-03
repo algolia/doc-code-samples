abf744fc37eba2bf19649c5b774b0ad84bdcf268
Priyesh2109 <49438857+Priyesh2109@users.noreply.github.com>
2020-07-03 12:22:58 +0100

    initial commit

---
+++ Vue+InstantSearch/places/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <app-places
+      :type="type"
+      :api-key="apiKey"
+      :app-id="appId"
+      :key="`${appId}-${apiKey}`"
+      @change="suggestion = $event.suggestion"
+    />
+    <HelloWorld/>
+    <Box/>
+  </div>
+</template>
+
+<script>
+import AppPlaces from "./components/Places";
+import HelloWorld from "./components/HelloWorld";
+import Box from "./components/Box";
+export default {
+  components: { AppPlaces, HelloWorld, Box },
+  data() {
+    return {
+      suggestion: undefined,
+      appId: undefined,
+      apiKey: undefined,
+      type: "city"
+    };
+  },
+  methods: {
+    toggleApiKey() {
+      if (this.apiKey) {
+        this.apiKey = undefined;
+        this.appId = undefined;
+      } else {
+        this.apiKey = "your-api-key";
+        this.appId = "your-appId-key";
+      }
+    },
+    toggleType() {
+      if (this.type === "city") {
+        this.type = "country";
+      } else {
+        this.type = "city";
+      }
+    }
+  }
+};
+</script>
---
+++ Vue+InstantSearch/places/src/components/Box.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="second">
+    <!--<h1>Where did you forget it?</h1>
+    <form>
+      <input type="text">
+    </form>-->
+    <p>
+      <b>Made with love ❤️</b>
+    </p>
+    <button type="button" class="btn btn-danger btn-lg btn-block">Check if item is found</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Box"
+};
+</script>
+
+<style scoped>
+.second h1 {
+  color: black;
+  margin-left: 520px;
+  margin-top: 100px;
+}
+
+.second p {
+  top: 10px;
+  position: absolute;
+  color: black;
+  font-size: 22px;
+}
+
+.second button {
+  margin-left: 450px;
+  width: 500px;
+  margin-top: 230px;
+  padding: 0.3em;
+  font-size: 22px;
+  color: white;
+  background-color: indianred;
+  border-radius: 9px;
+  height: 40px;
+}
+</style>
