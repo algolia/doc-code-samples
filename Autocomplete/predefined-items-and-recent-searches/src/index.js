a616bc5a375a248d6f955b2c905be41d10cd734e
eunjae-lee <karis612@gmail.com>
2020-10-20 17:48:54 +0200

    feat(autocomplete): add autocomplete example with predefined items and recent searches

---
+++ Autocomplete/predefined-items-and-recent-searches/src/index.js
@@ -0,0 +1,64 @@
+import algoliasearch from "algoliasearch/lite";
+import {
+  autocomplete,
+  getAlgoliaHits,
+  reverseHighlightItem,
+} from "@algolia/autocomplete-js";
+import { predefinedItems } from "./predefined-items";
+import { recentSearches } from "./recent-searches";
+
+const searchClient = algoliasearch(
+  "latency",
+  "6be0576ff61c053d5f9a3225e2a90f76"
+);
+
+autocomplete({
+  container: "#autocomplete",
+  plugins: [predefinedItems, recentSearches],
+  openOnFocus: true,
+  debug: true,
+  onStateChange({ state, prevState }) {
+    if (state.query !== prevState.query) {
+      console.log("query:", state.query);
+    }
+  },
+  getSources({ query }) {
+    return [
+      {
+        getInputValue: ({ suggestion }) => suggestion.query,
+        getSuggestions({ query }) {
+          return getAlgoliaHits({
+            searchClient,
+            queries: [
+              {
+                indexName: "instant_search_demo_query_suggestions",
+                query,
+                params: {
+                  hitsPerPage: 4,
+                  facetFilters: [...recentSearches.data.getFacetFilters()],
+                },
+              },
+            ],
+          }).then((results) => {
+            return results[0];
+          });
+        },
+        templates: {
+          header() {
+            if (query) {
+              return "";
+            }
+            return `
+              <div class="aa-HitsHeader">
+                <p>Search Results</p>
+              </div>
+            `;
+          },
+          item({ item }) {
+            return reverseHighlightItem({ item, attribute: "query" });
+          },
+        },
+      },
+    ];
+  },
+});
---
+++ Autocomplete/predefined-items-and-recent-searches/src/recent-searches.js
@@ -0,0 +1,25 @@
+import { createRecentSearchesPlugin } from "@algolia/autocomplete-plugin-recent-searches";
+const orgRecentSearches = createRecentSearchesPlugin({
+  key: "RECENT_SEARCH",
+  limit: 5,
+});
+
+export const recentSearches = {
+  ...orgRecentSearches,
+  getSources(params) {
+    const sources = orgRecentSearches.getSources(params);
+    return sources.map((source) => ({
+      ...source,
+      templates: {
+        ...source.templates,
+        header() {
+          return `
+            <div class="aa-RecentSearchesHeader">
+              <p>Recent Searches</p>
+            </div>
+          `;
+        },
+      },
+    }));
+  },
+};
