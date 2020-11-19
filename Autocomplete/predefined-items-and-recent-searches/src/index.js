ca220e0e32e59df9c51d0f61abbad0164439c31d
eunjae-lee <karis612@gmail.com>
2020-11-19 16:28:49 +0100

    new recent search implementation WIP

--- Autocomplete/predefined-items-and-recent-searches/src/index.js
+++ Autocomplete/predefined-items-and-recent-searches/src/index.js
@@ -2,62 +2,42 @@ import algoliasearch from 'algoliasearch/lite';
 import {
   autocomplete,
   getAlgoliaHits,
-  reverseHighlightItem,
+  highlightHit,
 } from '@algolia/autocomplete-js';
-import { predefinedItems } from './predefined-items';
-import { recentSearches } from './recent-searches';
+import { createLocalStorageRecentSearchesPlugin } from '@algolia/autocomplete-plugin-recent-searches';
+
+const recentSearches = createLocalStorageRecentSearchesPlugin({
+  key: 'RECENT_SEARCH',
+  limit: 5,
+});
 
 const searchClient = algoliasearch(
   'latency',
   '6be0576ff61c053d5f9a3225e2a90f76'
 );
+const index = searchClient.initIndex('instant_search');
 
 autocomplete({
   container: '#autocomplete',
-  plugins: [predefinedItems, recentSearches],
+  plugins: [recentSearches],
   openOnFocus: true,
-  onStateChange({ state, prevState }) {
-    if (state.query !== prevState.query) {
-      console.log('query:', state.query);
-    }
-  },
-  getSources({ query }) {
-    return [
-      {
-        getInputValue: ({ suggestion }) => suggestion.query,
-        getSuggestions({ query }) {
-          return getAlgoliaHits({
-            searchClient,
-            queries: [
-              {
-                indexName: 'instant_search_demo_query_suggestions',
-                query,
-                params: {
-                  hitsPerPage: 4,
-                  facetFilters: [...recentSearches.data.getFacetFilters()],
-                },
-              },
-            ],
-          }).then((results) => {
-            return results[0];
-          });
-        },
-        templates: {
-          header() {
-            if (query) {
-              return '';
-            }
-            return `
-              <div class="aa-HitsHeader">
-                <p>Search Results</p>
-              </div>
-            `;
-          },
-          item({ item }) {
-            return reverseHighlightItem({ item, attribute: 'query' });
-          },
-        },
-      },
-    ];
+  onSubmit({ state }) {
+    index.search(state.query).then((result) => {
+      console.log({
+        highlighted: highlightHit({ attribute: 'name', hit: result.hits[0] }),
+        result,
+      });
+      document.querySelector('#hits').innerHTML = result.hits
+        .map(
+          (hit) => `
+          <article class="hit">
+            <div class="image-wrapper"><img src="${hit.image}" /></div>
+            <p class="name">${hit._highlightResult.name.value}</p>
+            <p class="price">$${hit.price}</p>
+          </article>
+        `
+        )
+        .join('');
+    });
   },
 });
