c632a77d667ea84da94188d9f41655b2638bb5e5
Youcef Mammar <youcef.mammar@algolia.com>
2021-09-17 18:18:41 +0200

    chore(Angular 12): upgrade conditional-request example

--- Angular InstantSearch/conditional-request/src/app/app.module.ts
+++ Angular InstantSearch/conditional-request/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgAisModule } from 'angular-instantsearch';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [NgAisModule.forRoot(), BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    NgAisModule.forRoot(),
+    BrowserModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
