fbe12abbe468f5de0641433bd87cb7cc1f1b618f
Youcef Mammar <youcef.mammar@algolia.com>
2021-09-20 19:14:05 +0200

    run prettier

--- Angular InstantSearch/loading-indicator/src/app/loading-indicator.component.ts
+++ Angular InstantSearch/loading-indicator/src/app/loading-indicator.component.ts
@@ -1,29 +1,32 @@
-import {Component, Inject, forwardRef, Optional} from '@angular/core';
-import {TypedBaseWidget, NgAisInstantSearch, NgAisIndex} from 'angular-instantsearch';
+import { Component, Inject, forwardRef, Optional } from '@angular/core';
+import {
+  TypedBaseWidget,
+  NgAisInstantSearch,
+  NgAisIndex,
+} from 'angular-instantsearch';
 import connectSearchBox, {
   SearchBoxWidgetDescription,
-  SearchBoxConnectorParams
+  SearchBoxConnectorParams,
 } from 'instantsearch.js/es/connectors/search-box/connectSearchBox';
 
 @Component({
-  selector: "app-loading-indicator",
+  selector: 'app-loading-indicator',
   template: `
-      <div *ngIf="state.isSearchStalled && state.isSearchStalled">
-          Loading...
-      </div>
-  `
+    <div *ngIf="state.isSearchStalled && state.isSearchStalled">Loading...</div>
+  `,
 })
-export class LoadingIndicatorComponent extends TypedBaseWidget<SearchBoxWidgetDescription, SearchBoxConnectorParams> {
+export class LoadingIndicatorComponent extends TypedBaseWidget<
+  SearchBoxWidgetDescription,
+  SearchBoxConnectorParams
+> {
   public state: SearchBoxWidgetDescription['renderState'] = {
-    clear(): void {
-    },
+    clear(): void {},
     isSearchStalled: false,
-    query: "",
-    refine(value: string): void {
-    }
-  }
+    query: '',
+    refine(value: string): void {},
+  };
 
-// Rendering options
+  // Rendering options
   constructor(
     @Inject(forwardRef(() => NgAisIndex))
     @Optional()
