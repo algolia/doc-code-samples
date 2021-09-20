f68734dc11019e04e18feaae2e355f5390693453
Youcef Mammar <youcef.mammar@algolia.com>
2021-09-20 17:42:20 +0200

    chore(Angular IS): upgrade loading-indicator example to Angular 12

--- Angular InstantSearch/loading-indicator/src/app/loading-indicator.component.ts
+++ Angular InstantSearch/loading-indicator/src/app/loading-indicator.component.ts
@@ -1,42 +1,43 @@
-import { Component, Inject, forwardRef, OnInit } from "@angular/core";
-import { BaseWidget, NgAisInstantSearch } from "angular-instantsearch";
-
-const connectSearchMetaData = (renderFn, unmountFn) => (widgetParams = {}) => ({
-  init() {
-    renderFn({ searchMetadata: {} }, true);
-  },
-  render({ searchMetadata }) {
-    renderFn({ searchMetadata }, false);
-  },
-  dispose() {
-    unmountFn();
-  }
-});
+import {Component, Inject, forwardRef, Optional} from '@angular/core';
+import {TypedBaseWidget, NgAisInstantSearch, NgAisIndex} from 'angular-instantsearch';
+import connectSearchBox, {
+  SearchBoxWidgetDescription,
+  SearchBoxConnectorParams
+} from 'instantsearch.js/es/connectors/search-box/connectSearchBox';
 
 @Component({
   selector: "app-loading-indicator",
   template: `
-    <div>
-      <div *ngIf="state.searchMetadata && state.searchMetadata.isSearchStalled">
-        Loading...
+      <div *ngIf="state.isSearchStalled && state.isSearchStalled">
+          Loading...
       </div>
-    </div>
   `
 })
-export class LoadingIndicator extends BaseWidget implements OnInit {
-  state: {
-    searchMetadata: object;
-  };
-
-  constructor(
-    @Inject(forwardRef(() => NgAisInstantSearch))
-    public instantSearchParent
-  ) {
-    super("LoadingIndicator");
+export class LoadingIndicatorComponent extends TypedBaseWidget<SearchBoxWidgetDescription, SearchBoxConnectorParams> {
+  public state: SearchBoxWidgetDescription['renderState'] = {
+    clear(): void {
+    },
+    isSearchStalled: false,
+    query: "",
+    refine(value: string): void {
+    }
   }
 
-  public ngOnInit() {
-    this.createWidget(connectSearchMetaData as any, {});
+// Rendering options
+  constructor(
+    @Inject(forwardRef(() => NgAisIndex))
+    @Optional()
+    public parentIndex: NgAisIndex,
+    @Inject(forwardRef(() => NgAisInstantSearch))
+    public instantSearchInstance: NgAisInstantSearch
+  ) {
+    super('SearchBox');
+  }
+
+  ngOnInit() {
+    this.createWidget(connectSearchBox, {
+      // instance options
+    });
     super.ngOnInit();
   }
 }
