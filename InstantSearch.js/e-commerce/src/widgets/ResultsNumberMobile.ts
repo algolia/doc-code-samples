26746190da3f5536245c0e524509e59e18657d66
Clément Denoix <clement.denoix@algolia.com>
2021-10-11 11:55:07 +0200

    feat(InstantSearch.js) Add e-commerce demo (#269)

---
+++ InstantSearch.js/e-commerce/src/widgets/ResultsNumberMobile.ts
@@ -0,0 +1,8 @@
+import { stats } from 'instantsearch.js/es/widgets';
+
+export const resultsNumberMobile = stats({
+  container: '[data-widget="results-number-mobile"]',
+  templates: {
+    text: '<strong>{{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}}</strong> results',
+  },
+});
