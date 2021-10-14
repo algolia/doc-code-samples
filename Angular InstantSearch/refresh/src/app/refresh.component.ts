39f35c1ccfc1bc9542a9ce70d52957a630aa7038
Youcef Mammar <tkrugg@users.noreply.github.com>
2021-10-14 14:50:07 +0200

    chore(Angular IS): upgrade refresh example to Angular 12 (#262)

--- Angular InstantSearch/refresh/src/app/refresh.component.ts
+++ Angular InstantSearch/refresh/src/app/refresh.component.ts
@@ -1,39 +1,82 @@
-import { Component, Inject, forwardRef, OnInit } from "@angular/core";
-import { BaseWidget, NgAisInstantSearch } from "angular-instantsearch";
+import { Component, Inject, forwardRef, Optional, Input } from '@angular/core';
+import {
+  TypedBaseWidget,
+  NgAisInstantSearch,
+  NgAisIndex,
+} from 'angular-instantsearch';
+import { Connector } from 'instantsearch.js/es/types';
 
-const connectRefresh = (renderFn, unmountFn) => (widgetParams = {}) => ({
-  init() {
-    renderFn({ refresh() {} }, true);
-  },
-  render({ instantSearchInstance }) {
-    const refresh = instantSearchInstance.refresh.bind(instantSearchInstance);
-    renderFn({ refresh }, false);
-  },
-  dispose() {
-    unmountFn();
-  }
-});
+type RefreshWidgetDescription = {
+  $$type: 'custom.referesh';
+  renderState: {
+    refresh(): void;
+  };
+};
+type RefreshConnectorParams = {};
+
+type RefreshConnector = Connector<
+  RefreshWidgetDescription,
+  RefreshConnectorParams
+>;
+
+const connectRefresh: RefreshConnector =
+  (renderFn, unmountFn) => (widgetParams) => ({
+    $$type: 'custom.referesh',
+    init: function ({ instantSearchInstance }) {
+      renderFn(
+        {
+          refresh() {},
+          widgetParams,
+          instantSearchInstance,
+        },
+        true
+      );
+    },
+    render({ instantSearchInstance }) {
+      const refresh = instantSearchInstance.refresh.bind(instantSearchInstance);
+      renderFn(
+        {
+          refresh,
+          widgetParams,
+          instantSearchInstance,
+        },
+        false
+      );
+    },
+    dispose() {
+      if (unmountFn) {
+        unmountFn();
+      }
+    },
+  });
 
 @Component({
-  selector: "app-refresh",
-  template: `
-    <button (click)="state.refresh()">Refresh</button>
-  `
+  selector: 'app-refresh',
+  template: ` <button (click)="state.refresh()">Refresh</button> `,
 })
-export class Refresh extends BaseWidget implements OnInit {
-  state: {
-    refresh: Function;
+export class RefreshComponent extends TypedBaseWidget<
+  RefreshWidgetDescription,
+  RefreshConnectorParams
+> {
+  public state: RefreshWidgetDescription['renderState'] = {
+    refresh(): void {},
   };
 
+  // Rendering options
   constructor(
+    @Inject(forwardRef(() => NgAisIndex))
+    @Optional()
+    public parentIndex: NgAisIndex,
     @Inject(forwardRef(() => NgAisInstantSearch))
-    public instantSearchParent
+    public instantSearchInstance: NgAisInstantSearch
   ) {
-    super("Refresh");
+    super('Refresh');
   }
 
-  public ngOnInit() {
-    this.createWidget(connectRefresh as any, {});
+  ngOnInit() {
+    this.createWidget(connectRefresh, {
+      // instance options
+    });
     super.ngOnInit();
   }
 }
