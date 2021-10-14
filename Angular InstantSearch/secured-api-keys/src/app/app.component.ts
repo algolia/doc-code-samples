be1dcb9957bbe09742319a4b99ca7d122cf9bf48
Youcef Mammar <youcef.mammar@algolia.com>
2021-10-14 17:35:54 +0200

    chore(Angular InstantSearch): upgrade secured-api-keys example to Angular 12 (#257)

--- Angular InstantSearch/secured-api-keys/src/app/app.component.ts
+++ Angular InstantSearch/secured-api-keys/src/app/app.component.ts
@@ -1,19 +1,19 @@
-import { Component } from "@angular/core";
-import * as algoliasearch from "algoliasearch/lite";
+import { Component } from '@angular/core';
+import algoliasearch from 'algoliasearch/lite';
 
 const searchClient = algoliasearch(
-  "B1G2GM9NG0",
+  'B1G2GM9NG0',
   (window as any).SERVER_DATA.ALGOLIA_API_KEY
 );
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   config = {
+    indexName: 'demo_ecommerce',
     searchClient,
-    indexName: "demo_ecommerce"
   };
 }
