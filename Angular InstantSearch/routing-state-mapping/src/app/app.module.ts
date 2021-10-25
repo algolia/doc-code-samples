3d0052c7d40c2f4f214471965385c12d33a3143d
Youcef Mammar <youcef.mammar@algolia.com>
2021-10-25 21:37:17 +0200

    chore(Angular IS): upgrade routing-state-mapping example to Angular 12

--- Angular InstantSearch/routing-state-mapping/src/app/app.module.ts
+++ Angular InstantSearch/routing-state-mapping/src/app/app.module.ts
@@ -1,27 +1,26 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { NgAisModule } from "angular-instantsearch";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgAisModule } from 'angular-instantsearch';
+import { RouterModule, Routes } from '@angular/router';
 
-import { RouterModule, Routes } from "@angular/router";
-
-import { SearchComponent } from "./search/search.component";
-import { IndexComponent } from "./index/index.component";
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { IndexComponent } from './index/index.component';
 
 const appRoutes: Routes = [
-  { path: "search", component: SearchComponent },
-  { path: "", component: IndexComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'search/:category', component: SearchComponent },
+  { path: '', component: IndexComponent },
 ];
-
-import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent, SearchComponent, IndexComponent],
   imports: [
     NgAisModule.forRoot(),
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { enableTracing: true })
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
