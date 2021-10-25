df089fdc59d2e3af19da4bfd7e67913bbd2baa18
Youcef Mammar <tkrugg@users.noreply.github.com>
2021-10-25 22:15:14 +0200

    chore(Angular IS): upgrade routing-basic example to Angular 12 (#275)

--- Angular InstantSearch/routing-basic/src/app/app.component.ts
+++ Angular InstantSearch/routing-basic/src/app/app.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
-import * as algoliasearch from "algoliasearch/lite";
+import algoliasearch from 'algoliasearch/lite';
 
 const searchClient = algoliasearch(
-  "B1G2GM9NG0",
-  "aadef574be1f9252bb48d4ea09b5cfe5"
+  'B1G2GM9NG0',
+  'aadef574be1f9252bb48d4ea09b5cfe5'
 );
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   config = {
     indexName: 'demo_ecommerce',
     searchClient,
-    routing: true
+    routing: true,
   };
 }
