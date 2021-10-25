c1281b2bd0bd5f11b83b607abf5763458bde6a01
Youcef Mammar <youcef.mammar@algolia.com>
2021-10-25 21:45:30 +0200

    prettier

--- Angular InstantSearch/routing-state-mapping/src/app/search/search.component.ts
+++ Angular InstantSearch/routing-state-mapping/src/app/search/search.component.ts
@@ -1,44 +1,44 @@
-import { Component } from "@angular/core";
-import * as algoliasearch from "algoliasearch/lite";
+import { Component } from '@angular/core';
+import algoliasearch from 'algoliasearch/lite';
 
 const searchClient = algoliasearch(
-  "B1G2GM9NG0",
-  "aadef574be1f9252bb48d4ea09b5cfe5"
+  'B1G2GM9NG0',
+  'aadef574be1f9252bb48d4ea09b5cfe5'
 );
 
 @Component({
-  selector: "app-search",
-  templateUrl: "./search.component.html"
+  selector: 'app-search',
+  templateUrl: './search.component.html',
 })
 export class SearchComponent {
   config = {
-    indexName: "demo_ecommerce",
+    indexName: 'demo_ecommerce',
     searchClient,
     routing: {
       stateMapping: {
         stateToRoute(uiState) {
           return {
-            q: uiState.query || "",
+            q: uiState.query || '',
             brands:
               (uiState.refinementList &&
                 uiState.refinementList.brand &&
-                uiState.refinementList.brand.join("~")) ||
-              "all",
-            p: uiState.page || 1
+                uiState.refinementList.brand.join('~')) ||
+              'all',
+            p: uiState.page || 1,
           };
         },
         routeToState(routeState) {
-          if (routeState.brands === "all") routeState.brands = undefined;
+          if (routeState.brands === 'all') routeState.brands = undefined;
 
           return {
             query: routeState.q,
             refinementList: {
-              brand: routeState.brands && routeState.brands.split("~")
+              brand: routeState.brands && routeState.brands.split('~'),
             },
-            page: routeState.p
+            page: routeState.p,
           };
-        }
-      }
-    }
+        },
+      },
+    },
   };
 }
