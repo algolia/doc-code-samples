521ebe281e1eadaef89587755685ed6e1bc2c8ad
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-11-02 17:59:14 +0100

    feat(react-instantsearch): update routing basic example

--- React InstantSearch/routing-basic/src/App.js
+++ React InstantSearch/routing-basic/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
   InstantSearch,
   Hits,
@@ -12,7 +12,7 @@ import qs from 'qs';
 import PropTypes from 'prop-types';
 import './App.css';
 
-const DEBOUNCE_TIME = 700;
+const DEBOUNCE_TIME = 400;
 const searchClient = algoliasearch(
   'latency',
   '6be0576ff61c053d5f9a3225e2a90f76'
@@ -25,70 +25,58 @@ const searchStateToUrl = (props, searchState) =>
 
 const urlToSearchState = location => qs.parse(location.search.slice(1));
 
-class App extends Component {
-  state = {
-    searchState: urlToSearchState(this.props.location),
-    lastLocation: this.props.location,
-  };
+export function App({ location, history }) {
+  const [searchState, setSearchState] = useState(urlToSearchState(location));
+  const [debouncedSetState, setDebouncedSetState] = useState(null);
 
-  static getDerivedStateFromProps(props, state) {
-    if (props.location !== state.lastLocation) {
-      return {
-        searchState: urlToSearchState(props.location),
-        lastLocation: props.location,
-      };
-    }
+  function onSearchStateChange(updatedSearchState) {
+    clearTimeout(debouncedSetState);
 
-    return null;
+    setDebouncedSetState(
+      setTimeout(() => {
+        history.push(
+          updatedSearchState,
+          null,
+          searchStateToUrl(updatedSearchState)
+        );
+      }, DEBOUNCE_TIME)
+    );
+
+    setSearchState(updatedSearchState);
   }
 
-  onSearchStateChange = searchState => {
-    clearTimeout(this.debouncedSetState);
+  return (
+    <div className="container">
+      <InstantSearch
+        searchClient={searchClient}
+        indexName="instant_search"
+        searchState={searchState}
+        onSearchStateChange={onSearchStateChange}
+        createURL={createURL}
+      >
+        <div className="search-panel">
+          <div className="search-panel__filters">
+            <RefinementList attribute="brand" />
+          </div>
 
-    this.debouncedSetState = setTimeout(() => {
-      this.props.history.push(
-        searchStateToUrl(this.props, searchState),
-        searchState
-      );
-    }, DEBOUNCE_TIME);
+          <div className="search-panel__results">
+            <SearchBox className="searchbox" placeholder="Search" />
+            <Hits hitComponent={Hit} />
 
-    this.setState({ searchState });
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <InstantSearch
-          searchClient={searchClient}
-          indexName="instant_search"
-          searchState={this.state.searchState}
-          onSearchStateChange={this.onSearchStateChange}
-          createURL={createURL}
-        >
-          <div className="search-panel">
-            <div className="search-panel__filters">
-              <RefinementList attribute="brand" />
-            </div>
-
-            <div className="search-panel__results">
-              <SearchBox className="searchbox" placeholder="Search" />
-              <Hits hitComponent={Hit} />
-
-              <div className="pagination">
-                <Pagination />
-              </div>
+            <div className="pagination">
+              <Pagination />
             </div>
           </div>
-        </InstantSearch>
-      </div>
-    );
-  }
+        </div>
+      </InstantSearch>
+    </div>
+  );
 }
 
-function Hit(props) {
+function Hit({ hit }) {
   return (
     <div>
-      <Highlight attribute="name" hit={props.hit} />
+      <Highlight attribute="name" hit={hit} />
     </div>
   );
 }
