cfea2cdc5f35d7f78ee5a2c640f1c2883507c731
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2021-11-03 11:19:03 +0100

    refactor: re-order imports

--- React InstantSearch/routing-basic/src/index.js
+++ React InstantSearch/routing-basic/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { App } from './App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { App } from './App';
+import './index.css';
 
 ReactDOM.render(
   <Router>
