9963552ceb348913eb2d271f17e74f189d25df5a
Youcef Mammar <youcef.mammar@algolia.com>
2021-11-09 22:18:10 +0100

    fix(Angular InstantSearch): upgrade geo-search example
    - used create-instantsearch-app to generate an Angular 12 boilerplate
    - added [`@angular/google-maps`](https://github.com/angular/components/tree/master/src/google-maps) replacing [`@agm/core`](https://github.com/algolia/doc-code-samples/blob/master/Angular%20InstantSearch/geo-search/package.json#L14) which turned out to be incompatible with Angular 12

--- Angular InstantSearch/geo-search/src/app/app.component.ts
+++ Angular InstantSearch/geo-search/src/app/app.component.ts
@@ -1,21 +1,19 @@
-import { Component } from "@angular/core";
-import * as algoliasearch from "algoliasearch/lite";
+import { Component } from '@angular/core';
+import algoliasearch from 'algoliasearch/lite';
 
 const searchClient = algoliasearch(
-  "latency",
-  "6be0576ff61c053d5f9a3225e2a90f76"
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
 );
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public config = {
-    indexName: "airports",
-    searchClient
+  config = {
+    indexName: 'airports',
+    searchClient,
   };
-
-  public searchParameters = { query: "" };
 }
