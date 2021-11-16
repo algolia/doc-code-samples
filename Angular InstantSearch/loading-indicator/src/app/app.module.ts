6ed8e605dc8b699902ac700268b42a20fea8c8a9
Youcef Mammar <tkrugg@users.noreply.github.com>
2021-11-16 11:36:34 +0100

    chore(Angular IS): upgrade loading-indicator example to Angular 12 (#261)

--- Angular InstantSearch/loading-indicator/src/app/app.module.ts
+++ Angular InstantSearch/loading-indicator/src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { NgAisModule } from "angular-instantsearch";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgAisModule } from 'angular-instantsearch';
 
-import { AppComponent } from "./app.component";
-import { LoadingIndicator } from "./loading-indicator.component";
+import { AppComponent } from './app.component';
+import { LoadingIndicatorComponent } from "./loading-indicator.component";
 
 @NgModule({
-  declarations: [AppComponent, LoadingIndicator],
-  imports: [NgAisModule.forRoot(), BrowserModule],
+  declarations: [
+    AppComponent,
+    LoadingIndicatorComponent,
+  ],
+  imports: [
+    NgAisModule.forRoot(),
+    BrowserModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
