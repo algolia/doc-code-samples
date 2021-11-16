a14fa0457ecb18e7bba7ffa60bdb920477bf1db1
Youcef Mammar <youcef.mammar@algolia.com>
2021-11-16 11:00:47 +0100

    prettier

--- Angular InstantSearch/conditional-request/src/app/app.component.ts
+++ Angular InstantSearch/conditional-request/src/app/app.component.ts
@@ -1,14 +1,20 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import algoliasearch from 'algoliasearch/lite';
-import {MultipleQueriesQuery, MultipleQueriesResponse, SearchResponse} from '@algolia/client-search';
+import {
+  MultipleQueriesQuery,
+  MultipleQueriesResponse,
+  SearchResponse,
+} from '@algolia/client-search';
 
 const originalSearchClient = algoliasearch(
   'B1G2GM9NG0',
   'aadef574be1f9252bb48d4ea09b5cfe5'
 );
 
-const search: typeof originalSearchClient.search = <THit>(queries: readonly MultipleQueriesQuery[]) => {
-  if (queries.every(({params}) => !params || !params.query)) {
+const search: typeof originalSearchClient.search = <THit>(
+  queries: readonly MultipleQueriesQuery[]
+) => {
+  if (queries.every(({ params }) => !params || !params.query)) {
     return Promise.resolve({
       results: queries.map(() => ({
         exhaustiveNbHits: false,
@@ -17,9 +23,9 @@ const search: typeof originalSearchClient.search = <THit>(queries: readonly Mult
         nbHits: 0,
         nbPages: 0,
         page: 0,
-        params: "",
+        params: '',
         processingTimeMS: 0,
-        query: ""
+        query: '',
       })),
     });
   }
@@ -29,13 +35,13 @@ const search: typeof originalSearchClient.search = <THit>(queries: readonly Mult
 
 const searchClient = {
   ...originalSearchClient,
-  search
-}
+  search,
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   config = {
