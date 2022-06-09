1235dc65c90be00129251646c13728f5f1d059b6
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-06-09 10:33:00 +0200

    fix: avoid using strict mode until we fully support it

--- React InstantSearch Hooks/multi-index-hits/src/index.js
+++ React InstantSearch Hooks/multi-index-hits/src/index.js
@@ -8,8 +8,4 @@ import './index.css';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
