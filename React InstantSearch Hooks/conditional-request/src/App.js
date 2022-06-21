4af14ef5419ef5ad724c93eca9d3997e00322948
Haroen Viaene <hello@haroen.me>
2022-06-21 11:12:29 +0200

    feat(hooks): document conditional requests (#393)
    Same example client modification as for react instantsearch

---
+++ React InstantSearch Hooks/conditional-request/src/App.js
@@ -0,0 +1,54 @@
+import algoliasearch from 'algoliasearch/lite';
+import {
+  Highlight,
+  Hits,
+  InstantSearch,
+  SearchBox,
+} from 'react-instantsearch-hooks-web';
+
+import 'instantsearch.css/themes/algolia.css';
+import './App.css';
+
+const algoliaClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+const searchClient = {
+  ...algoliaClient,
+  search(requests) {
+    if (requests.every(({ params }) => !params.query)) {
+      return Promise.resolve({
+        results: requests.map(() => ({
+          hits: [],
+          nbHits: 0,
+          nbPages: 0,
+          processingTimeMS: 0,
+        })),
+      });
+    }
+
+    return algoliaClient.search(requests);
+  },
+};
+
+function App() {
+  return (
+    <div className="container">
+      <InstantSearch indexName="instant_search" searchClient={searchClient}>
+        <SearchBox />
+        <Hits hitComponent={Hit} />
+      </InstantSearch>
+    </div>
+  );
+}
+
+function Hit({ hit }) {
+  return (
+    <div>
+      <Highlight attribute="name" hit={hit} />
+    </div>
+  );
+}
+
+export default App;
