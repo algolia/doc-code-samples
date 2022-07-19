1489496e0f4202b8e700abd238f6dff2c8ea905e
Dhaya <154633+dhayab@users.noreply.github.com>
2022-07-19 14:30:04 +0200

    feat(hooks): document geo search (#399)
    Co-authored-by: François Chalifour <francoischalifour@users.noreply.github.com>

---
+++ react-instantsearch-hooks/geo-search/src/Airports.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { Marker, Popup, useMapEvents } from 'react-leaflet';
+import { DivIcon } from 'leaflet';
+import { useSearchBox } from 'react-instantsearch-hooks-web';
+import { useGeoSearch } from './useGeoSearch';
+
+import type { GeoHit } from 'instantsearch.js/es/connectors/geo-search/connectGeoSearch';
+
+export function Airports() {
+  const { query, refine: refineQuery } = useSearchBox();
+  const {
+    items,
+    refine: refineItems,
+    currentRefinement,
+    clearMapRefinement,
+  } = useGeoSearch();
+
+  const [previousQuery, setPreviousQuery] = useState(query);
+  const [skipViewEffect, setSkipViewEffect] = useState(false);
+
+  // When the user moves the map, we clear the query if necessary to only
+  // refine on the new boundaries of the map.
+  const onViewChange = ({ target }) => {
+    setSkipViewEffect(true);
+
+    if (query.length > 0) {
+      refineQuery('');
+    }
+
+    refineItems({
+      northEast: target.getBounds().getNorthEast(),
+      southWest: target.getBounds().getSouthWest(),
+    });
+  };
+
+  const map = useMapEvents({
+    zoomend: onViewChange,
+    dragend: onViewChange,
+  });
+
+  // When the query changes, we remove the boundary refinement if necessary and
+  // we center the map on the first result.
+  if (query !== previousQuery) {
+    if (currentRefinement) {
+      clearMapRefinement();
+    }
+
+    // `skipViewEffect` allows us to bail out of centering on the first result
+    // if the query has been cleared programmatically.
+    if (items.length > 0 && !skipViewEffect) {
+      map.setView(items[0]._geoloc);
+    }
+
+    setSkipViewEffect(false);
+    setPreviousQuery(query);
+  }
+
+  return (
+    <>
+      {items.map((item) => (
+        <Marker
+          key={item.objectID}
+          position={item._geoloc}
+          icon={createAirportIcon(item)}
+        >
+          <Popup>
+            <strong>{item.name}</strong>
+            <br />
+            {item.city}, {item.country}
+          </Popup>
+        </Marker>
+      ))}
+    </>
+  );
+}
+
+function createAirportIcon(item: GeoHit) {
+  return new DivIcon({
+    html: `<div class="marker">${item.airport_id}</div>`,
+    popupAnchor: [0, -15],
+  });
+}
