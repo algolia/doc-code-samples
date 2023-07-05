0db12f36db4262409e02f7fe5dfa027176f3b732
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2023-07-05 10:48:19 +0200

    feat(React InstantSearch Hooks): introduce range slider example with React Spectrum (#457)

---
+++ react-instantsearch-hooks/range-slider-with-react-spectrum/src/App.js
@@ -0,0 +1,47 @@
+import {
+  Hits,
+  InstantSearch,
+  Pagination,
+  RefinementList,
+  SearchBox,
+} from 'react-instantsearch-hooks-web';
+import algoliasearch from 'algoliasearch/lite';
+
+import { RangeSlider } from './RangeSlider';
+
+import './App.css';
+
+const searchClient = algoliasearch(
+  'B1G2GM9NG0',
+  'aadef574be1f9252bb48d4ea09b5cfe5'
+);
+
+function App() {
+  return (
+    <div className="container">
+      <InstantSearch
+        searchClient={searchClient}
+        indexName="demo_ecommerce"
+        insights={true}
+      >
+        <div className="search-panel">
+          <div className="search-panel__filters">
+            <RefinementList attribute="brand" />
+            <RangeSlider attribute="price" />
+          </div>
+
+          <div className="search-panel__results">
+            <SearchBox className="searchbox" placeholder="Search" />
+            <Hits />
+
+            <div className="pagination">
+              <Pagination />
+            </div>
+          </div>
+        </div>
+      </InstantSearch>
+    </div>
+  );
+}
+
+export default App;
