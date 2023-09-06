e673ca453787e923cbe90a2ecb8d5e8dfa6a608d
Dhaya <154633+dhayab@users.noreply.github.com>
2023-09-06 11:42:48 +0200

    remove indexing source code for java,php,scala

--- onboarding/src/indexing-relevance/scala/src/main/scala/com/algolia/Product.scala
+++
@@ -1,9 +0,0 @@
-package com.algolia
-
-case class Product(name: String,
-                   description: String,
-                   brand: String,
-                   categories: Iterable[String],
-                   price: Double,
-                   image: String,
-                   popularity: Integer)
