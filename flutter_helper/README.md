9f58257f2c23a2d45facb994ef05b9ea954003b7
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2025-09-02 16:36:06 +0200

    feat(flutter): add getting started example

---
+++ flutter_helper/README.md
@@ -0,0 +1,3 @@
+# flutter_helper
+
+A new Flutter project.
---
+++ flutter_helper/android/app/src/main/kotlin/com/example/flutter_helper/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_helper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
